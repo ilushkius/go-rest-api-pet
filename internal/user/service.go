d349e9bea024f1557dfa6cd4ffc727ca8aa6c299
Ilya.Gunderin <Ilya.Gunderin@leroymerlin.ru>
2022-08-15 13:07:36 +0500

    add MongoDB 1

--- internal/user/service.go
+++ internal/user/service.go
@@ -1,1 +1,16 @@
 package user
+
+import (
+	"context"
+	"go-rest-api-pet/pkg/logging"
+)
+
+type Service struct {
+	storage Storage
+	logger  *logging.Logger
+}
+
+func (s *Service) Create(ctx context.Context, dto CreateUserDTO) (u User, err error) {
+	//TODO for the next one
+	return
+}
